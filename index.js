e9b98c289afb7b72484625c93c92fe69abc0f1d1
Sayem <sayem465508@gmail.com>
2024-04-29 22:49:59 +0600

    updated the vercel.json file and fix the cors error

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('user')
 
     app.get('/coffee', async(req, res) => {
         const cursor = coffeeCollection.find();
@@ -73,6 +74,41 @@ async function run() {
       const result = await coffeeCollection.deleteOne(query);
       res.send(result)
     })
+
+    // User related apis
+    app.get('/user', async(req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/user', async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.patch('/user', async(req, res) => {
+      const user = req.body;
+      const filter = {email: user.email};
+      const updatedUser = {
+        $set: {
+          lastSignIn: user.lastSignIn,
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedUser);
+      res.send(result)
+    })
+
+    app.delete('/user/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
