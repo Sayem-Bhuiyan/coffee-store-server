2b3f6056f30b9f8a101ff99b2e8c41e822313258
Sayem <sayem465508@gmail.com>
2024-05-01 21:21:12 +0600

    updated the ueser

--- index.js
+++ index.js
@@ -101,6 +101,8 @@ async function run() {
       res.send(result)
     })
 
+    
+
     app.delete('/user/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
